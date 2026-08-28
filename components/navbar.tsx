93e8bbc1a4b21f5a3d823741dc7837f59789c18a
snowydevd <lautarochini4@gmail.com>
2026-08-28 17:39:17 -0300

    add shadcn and navigation menu

---
+++ components/navbar.tsx
@@ -0,0 +1,8 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
